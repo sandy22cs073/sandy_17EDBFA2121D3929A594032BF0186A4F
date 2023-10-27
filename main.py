6d2dfbedd9442162f9e5095455e5a5767d8b6c71
sandy22cs073 <148530587+sandy22cs073@users.noreply.github.com>
2023-10-27 12:03:49 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,20 +1,34 @@
-#
-class Player:
-    def play(self):
-        print("The player is playing cricket")
+class BankAccount:
+  def __init__(self, account_number, account_holder_name, initial_balance=0):
+      self.__account_number = account_number
+      self.__account_holder_name = account_holder_name
+      self.__account_balance = initial_balance
 
-class Batsman(Player):
-    def play(self):
-        print("The batsman is batting")
+  def deposit(self, amount):
+      if amount > 0:
+          self.__account_balance += amount
+          return f"Deposited ₹{amount}. New balance: ₹{self.__account_balance}"
+      else:
+          return "Invalid deposit amount. Please enter a positive amount."
 
-class Bowler(Player):
-    def play(self):
-        print("The bowler is bowling")
+  def withdraw(self, amount):
+      if 0 < amount <= self.__account_balance:
+          self.__account_balance -= amount
+          return f"Withdrew ₹{amount}. New balance: ₹{self.__account_balance}"
+      elif amount > self.__account_balance:
+          return "Insufficient funds."
+      else:
+          return "Invalid withdrawal amount. Please enter a positive amount."
 
-# Create objects of Batsman and Bowler classes
-batsman = Batsman()
-bowler = Bowler()
+  def display_balance(self):
+      return f"Account balance for {self.__account_holder_name}: ₹{self.__account_balance}"
 
-# Call the play() method for each object
-batsman.play()
-bowler.play()
+# Testing the BankAccount class
+if __name__ == "__main__":
+  account1 = BankAccount("123456789", "Ganeshkumar", 1000)
+
+  print(account1.display_balance())
+  print(account1.deposit(500))
+  print(account1.withdraw(200))
+  print(account1.withdraw(1500))
+  print(account1.display_balance())
